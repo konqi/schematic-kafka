53b1e771211333065514a189adcbe384df7e5251
Johann Wagner <johann.wagner@codecentric.de>
2021-05-08 22:10:28 +0200

    remove todo

--- src/kafka-helper.spec.ts
+++ src/kafka-helper.spec.ts
@@ -1,14 +1,13 @@
 import { kafkaDecode, kafkaEncode } from "./kafka-helper"
 
 it("encodes/decodes", () => {
-  // TODO
   const result = kafkaEncode(1, Buffer.from("Hello World!"))
   const { schemaId, payload } = kafkaDecode(result)
   expect(schemaId).toEqual(1)
   expect(payload.toString()).toEqual("Hello World!")
 })
 
-it("can handle no schema decodes", () => {
+it("can handle payloads that don't use the schema-notation", () => {
   const { schemaId, payload } = kafkaDecode(Buffer.from("Hello World!"))
   expect(schemaId).toBeUndefined()
   expect(payload.toString()).toEqual("Hello World!")

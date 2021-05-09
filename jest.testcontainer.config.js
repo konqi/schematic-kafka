0878cf5d3aed193c23c8499bd4863fc5f8ea096f
Johann Wagner <johann.wagner@codecentric.de>
2021-05-09 16:00:38 +0200

    separate unit tests from testcontainers tests

---
+++ jest.testcontainer.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.spec.json",
+    },
+  },
+  testRegex: "(\\.|\\/)testcontainers(test|spec)\\.[jt]sx?$",
+}

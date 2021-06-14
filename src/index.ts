0cdb8aba11c3e5f6096ec7fe153037eff9e7a459
Johann Wagner <johann.wagner@codecentric.de>
2021-06-14 13:28:14 +0200

    expose kafkaDecode and kafkaEncode

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { KafkaRegistryHelper } from "./kafka-registry-helper"
 export { SchemaRegistryClient, SchemaType } from "./schema-registry-client"
+export { kafkaDecode, kafkaEncode } from "./kafka-helper"
